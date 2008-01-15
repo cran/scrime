86e2294559ffb52f768819f3440af7816953b7bb
Holger Schwender <holger.schw@gmx.de>
2008-01-15 00:00:00 +0000

    version 1.0.0

---
+++ R/evalRedundancy.R
@@ -0,0 +1,24 @@
+`evalRedundancy` <-
+function(mat,ia,red=0){
+	vec.snps<-strsplit(ia," & ")[[1]]
+	n.snps<-length(vec.snps)
+	if(n.snps==1)
+		return(ia)
+	vec.jack<-character(n.snps)
+	for(i in 1:n.snps)
+		vec.jack[i]<-paste(vec.snps[-i],collapse=" & ")
+	vec.red<-numeric(n.snps)
+	attach(mat)
+	full<-sum(eval(parse(text=ia)))
+	for(i in 1:n.snps)
+		vec.red[i]<-sum(eval(parse(text=vec.jack[i])))
+	detach(mat)
+	if(any(vec.red-full<=red)){
+		warning("A redundant SNP in the explanatory interactions is removed.",call.=FALSE)
+		ids<-which.min(vec.red-full)[1]
+		new.ia<-vec.jack[ids]
+		ia<-evalRedundancy(mat,new.ia,red=red)
+	}
+	ia
+}
+
